6c2f98a913ecd678a21c1a0733a7432a20f67b18
volodymyrzdrok <58653496+volodymyrzdrok@users.noreply.github.com>
2023-01-05 10:50:39 +0000

    Deploying to gh-pages from @ volodymyrzdrok/js-team-project@5449555b430831cc8fca5c5b7354e83976df88ae 🚀

--- partials/favorite-ingredients.9050ae46.js
+++
@@ -1,2 +0,0 @@
-console.log("працює INGREDDD");
-//# sourceMappingURL=favorite-ingredients.9050ae46.js.map
--- partials/favorite-ingredients.9657460c.js
+++
@@ -1,2 +0,0 @@
-console.log("працює INGREDDD");
-//# sourceMappingURL=favorite-ingredients.9657460c.js.map
