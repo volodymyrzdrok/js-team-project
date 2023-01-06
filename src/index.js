317502b0cff116ad719341dae6e713bd11dbdbe2
RomanSyhlianyk <romansyglianyk@gmail.com>
2023-01-06 13:39:53 +0200

    +++

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ import { getCocktailsByLetter } from './js/getProducts/getCocktailsByLetter';
 // refs.letter.addEventListener('click', getCocktailsByLetter);
 refs.searchCocktailsInput[0].addEventListener('submit', getCocktailsByName);
 refs.searchCocktailsInput[1].addEventListener('submit', getCocktailsByName);
-// getRandomCocktails();
+getRandomCocktails();
--- src/js/getProducts/getCocktailsByName.js
+++ src/js/getProducts/getCocktailsByName.js
@@ -1,7 +1,7 @@
 import { fetchProductsByName } from '../api/api';
 import { refs } from '../refs/refs';
 import { markupProductsList } from '../templates/markupSearchingCocktails';
-import img from '../../images/Not-found.png'
+import img from '../../images/not-found-img/Not-found.png';
 export async function getCocktailsByName(e) {
   console.log(e);
   e.preventDefault();
@@ -11,8 +11,27 @@ export async function getCocktailsByName(e) {
   if (data.drinks === null) {
     refs.productTitle.textContent =
       'Sorry, we didn\'t find any cocktail for you';
-    refs.productList.innerHTML =
-      `<img class="not-found" src="${img}" alt="" width="280" height="280"></img>`;
+    refs.productList.innerHTML = `<picture>
+                <source
+                  srcset="${img}"
+                  media="(min-width: 1200px)"
+                />
+                <source
+                  srcset="${img} "
+                  media="(min-width: 768px)"
+                />
+                <img
+                  class="our-team__img"
+                  width="270"
+                  srcset="
+                    ${img}   1x,
+                    ${img} 2x,
+                    ${img}3x
+                  "
+                  src="${img}"
+                  alt="cafe"
+                />
+              </picture>`;
     e.target.reset();
     return;
   } else {
