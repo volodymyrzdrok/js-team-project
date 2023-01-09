b889b624ce6717f6011f939cddc204ad9aca2888
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-01-09 12:27:14 +0200

    add favicons

---
+++ src/js/theme/theme-controller.js
@@ -0,0 +1,23 @@
+import { refs } from '../refs/refs';
+import { THEME_DARK, THEME_SWITCHER } from '../utils/constans';
+
+const onDarkTheme = localStorage.getItem(THEME_SWITCHER);
+
+export function themeOn() {
+  if (onDarkTheme) {
+    refs.bodyRef.classList.add(THEME_DARK);
+    refs.checkboxInput.checked = true;
+  }
+}
+
+export function themeController(e) {
+  const checkedSwitcher = e.target.checked;
+
+  if (checkedSwitcher) {
+    localStorage.setItem(THEME_SWITCHER, JSON.stringify(THEME_DARK));
+    refs.bodyRef.classList.add(THEME_DARK);
+  } else {
+    localStorage.removeItem(THEME_SWITCHER);
+    refs.bodyRef.classList.remove(THEME_DARK);
+  }
+}
