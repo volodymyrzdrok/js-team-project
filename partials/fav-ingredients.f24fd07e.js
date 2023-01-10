5e20303d7613125f51fdbfe556b63762802d2eaf
volodymyrzdrok <58653496+volodymyrzdrok@users.noreply.github.com>
2023-01-10 17:51:14 +0000

    Deploying to gh-pages from @ volodymyrzdrok/js-team-project@e748b41a9d8dcc0a454d32311b275a4aa0260fb0 🚀

--- partials/fav-ingredients.f24fd07e.js
+++
@@ -1,2 +0,0 @@
-var refs={openMenuBtn:document.querySelector(".mobile-menu__burger"),closeMenuBtn:document.querySelector(".mobile-menu__close-btn"),menu:document.querySelector(".mobile-menu")};function toggleModal(){refs.menu.classList.toggle("is-open")}refs.openMenuBtn.addEventListener("click",toggleModal),refs.closeMenuBtn.addEventListener("click",toggleModal);
-//# sourceMappingURL=fav-ingredients.f24fd07e.js.map
